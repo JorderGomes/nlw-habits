31d9133ead92ea2f703c3379b9f0a4a20e05562f
JorderGomes <jordernoveau@gmail.com>
2023-01-21 13:56:39 -0300

    controle de dados

--- habit front/scripts/index.js
+++ habit front/scripts/index.js
@@ -1,13 +1,27 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ['01-01', '01-02', '01-06','01-07','01-08','01-09','01-10','01-11','01-12','01-13',],
-    water: ['04-01', '01-04', '01-08'],
-    food: ['01-09'],
+
+
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add(){
+    const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
+    if (nlwSetup.dayExists(today)){
+        alert('Dia já incluso.')
+        return
+    }
+    nlwSetup.addDay(today)
 }
 
+function save(){
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
-
 
